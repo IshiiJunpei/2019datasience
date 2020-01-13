f691f86bd2c11edf2dd16a51b30487395dabdae9
IshiiJunpei <junpei9@gmail.com>
2020-01-13 09:57:24 +0900

    奈文研報告書掲載用図版をpdfで作成　fig_for_book/に格納

--- fig_for_book/book.R
+++ fig_for_book/book.R
@@ -6,7 +6,10 @@ library(ggthemes)
 #北海道恵庭市西島松5遺跡出土の奈良時代の刀剣類のデータを使用します。
 
 iron<-read.csv("../data/iron.csv")
-iron[,c(4:12)]%>%head()%>%kable()
+＃ 一覧表出力
+iron[,c(4:12)] %>% write.csv("iron_head.csv")
+
+# ヒストグラム
 p<-iron%>%
 	ggplot(aes(x=刀身長))+
 	geom_histogram() +
@@ -40,137 +43,142 @@ p <- iron%>%
   )	
 ggsave("02sword_scat.pdf",p,width=5.8,height=3.6,family="Japan1GothicBBB") 
 
-## ヒストグラムを使うべき理由
 
-ヒストグラムのもう一つの利点は、分布の形状を数的モデルに近似して比較できることです。
-次の図は正規曲線を重ねた刀身長のヒストグラムです。
-
-```{r fig.width=10, fig.height=5,warning=FALSE,error=FALSE}
+## ヒストグラム+正規曲線
 # 正規曲線作成のための統計量算出
 iron%>%
 	summarise(mean=mean(刀身長,na.rm=T),sd=sd(刀身長,na.rm=T)) -> s_iron
-kable(s_iron)
-```
-	
-```{r fig.width=10, fig.height=5,warning=FALSE,error=FALSE}
 # 正規曲線作成
 x<-seq(0, 60, 0.1)
 nom <- x%>%dnorm(mean=s_iron$mean, sd=s_iron$sd)
 nom2<-data.frame(X=x,Y=nom)
 #正規曲線付きヒストグラム
-iron%>%
+p <- iron%>%
 	ggplot(aes(x=刀身長,y=..density..))+
 		geom_histogram()+
 		geom_line(data=nom2,aes(x=x,y=Y))+
 		scale_colour_ptol()+
-		theme_minimal()
-```
-
-刀身長のヒストグラムと正規分布曲線を重ねることによって、刀身長の分布が正規分布から大きく外れていることがはっきりします。
-これは、散布図では絶対に表現できません。
-上記のヒストグラムから、古代の刀剣に複数のサイズ規範がある可能性を指摘することはできそうです。
-
-## なぜヒストグラムは使われないのだろうか？
-理由の一つとして、ヒストグラムのもつ「数的モデルとの近似が容易である」という特性を考古学の研究者が活かせていない、ということが考えられます。
-例えば正規分布に対する理解や正規分布で近似できるということはどのような意味をもつのか、そのような判断が難しいのだろうと思います。
-
-## エクセルでヒストグラム
-「エクセルでヒストグラムを作りにくい」ということも理由の一つかもしれません。
-エクセルでヒストグラムを作れないわけではないのですが、度数分布表から棒グラフを作成することになるので、一手間かかります。
-
-ビン幅の調整をするにも、いちいち度数分布表を作り直さないといけない、ということも面倒です。
-こうした理由でヒストグラムが敬遠されるではないかと感じています。
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=9),
+  		legend.text = element_text(size=9),
+  		axis.title.y= element_blank(),
+  		axis.title.x = element_text(size=6),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=9),
+     panel.grid=element_blank(),
+  )	
+ggsave("03sword_hist_norm.pdf",p,width=5.8,height=2.8,family="Japan1GothicBBB") 
 
 # 箱ひげ図を用いた連続量の比較
-
-以下の手順でダミーデータを生成します。
-```{r warning=FALSE,error=FALSE,message=FALSE,fig.width=10, fig.height=7}
-# irisデータ読み込み
+# ダミーデータ生成
 data<-iris
-#ダミーデータ生成
 pot<-data[,c(1,2,5)]
 colnames(pot)<-c("器高","口径","分類")
 pot$分類<-factor(pot$分類,levels=c("setosa","versicolor","virginica"),
 	labels=c("A型","B型","C型"))
 pot$器高<-pot$器高*7
 pot$口径<-pot$口径*10
-pot%>%head()%>%kable()
-```
 
-連続量の分布や差を、離散量ごとに比較します。
-連続量と離散量の組み合わせのデータとは、例えば土器分類ごとに口径の分布を確認するようなケースです。次に紹介するように有力な方法がいくつか存在しますが、本命は**箱ひげ図**です。
-
-<br>
-
-## ヒストグラム
-
-```{r fig.width=10, fig.height=6,warning=FALSE,error=FALSE}
-pot%>%
+# ヒストグラム
+p <- pot%>%
 	ggplot(aes(x=口径,fill=分類))+
 		geom_histogram()+
-		scale_fill_ptol()+
+		scale_fill_viridis_d()+
 		facet_wrap(~分類,ncol=1,scales="free_y")+
-		theme_minimal()
-```
-
-<br>
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=9),
+  		legend.text = element_text(size=9),  
+      legend.position = 'none' ,
+  		axis.title.y= element_blank(),
+  		axis.title.x = element_text(size=6),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank(),
+  )	
+ggsave("04pot_hist_facet.pdf",p,width=5.8,height=3.4,family="Japan1GothicBBB") 
 
 ## 密度図
-
-```{r fig.width=10, fig.height=4,warning=FALSE,error=FALSE}
-pot%>%
+p <- pot%>%
 	ggplot(aes(x=口径,fill=分類))+
 		geom_density(alpha=0.7)+
-		scale_fill_ptol()+
-		theme_minimal()
-```
-
-<br>
+  	scale_fill_viridis_d() +
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6),  
+  		axis.title.y= element_blank(),
+  		axis.title.x = element_text(size=6),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank(),
+  )	
+ggsave("05pot_density.pdf",p,width=5.8,height=2.6,family="Japan1GothicBBB") 
 
 ## 箱ひげ図
-
-```{r fig.width=10, fig.height=3,warning=FALSE,error=FALSE}
 library(ggforce)
-pot%>%
+p <- pot%>%
 	ggplot(aes(x=分類,y=口径,fill=分類))+	
 		geom_boxplot(alpha = 0.2)+		#不透明度を0.2
     	geom_sina(aes(colour = 分類),		#geom_sina()関数でaes()の引数にcolour=分類を指定
               alpha = 0.4, 
-              size = 3) +
+              size = 1.5) +
 		scale_fill_viridis_d() +		#viridis_d(は連続量、離散量ならviridis_c()を指定する
   		scale_colour_viridis_d() +
 		coord_flip()+
-		theme_minimal()
-```
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6),  
+  		axis.title.y= element_blank(),
+      legend.position = 'none' ,
+  		axis.title.x = element_text(size=6),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank(),
+  )	
+ggsave("06pot_box.pdf",p,width=5.8,height=1.8,family="Japan1GothicBBB") 
 
-分類ごとの口径の差を確認する目的では箱ひげ図がもっとも敏感に差を可視化してくれます。分布の形状に注目したい場合はヒストグラムや密度図も有力な手法となります。
+## バイオリンプロット
+p <- pot%>%
+	ggplot(aes(x=分類,y=口径,fill=分類))+	
+		geom_violin(scale = "count" , alpha = 0.2,trim = FALSE)+		#不透明度を0.2 面積を実数に比例、端をなめらかに処理
+    	geom_sina(aes(colour = 分類),		#geom_sina()関数でaes()の引数にcolour=分類を指定
+              alpha = 0.4, 
+              size = 1.5) +
+		scale_fill_viridis_d() +		#viridis_d(は連続量、離散量ならviridis_c()を指定する
+  		scale_colour_viridis_d() +
+		coord_flip()+
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6), 
+       legend.position = 'none' ,
+  		axis.title.y= element_blank(),
+  		axis.title.x = element_text(size=6),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank(),
+  )	
+ggsave("07pot_violin.pdf",p,width=5.8,height=2.5,family="Japan1GothicBBB") 
 
-## 参考〜多重比較による差の検定〜
 
-箱ひげ図などの可視化手法によって、土器の口径は分類ごとに差がありそうだということがわかりました。
-差があるかどうかを定量的に判断するために統計的な検定を行います。
-
-この場合、3つの群に分類されていますので、3つの群同士に差があるかどうかを統計的に確かめることになります。
-多群の差の検定手法の一つである「多重比較」を行います。
-
-### 分散分析
-
-最初に分散分析で品種によって差があるかどうかを確認します。
-p値が2.2e-16と極めて小さい値をとることから、分類によって差があることがわかります。
-
-```{r ,warning=FALSE,error=FALSE}
+## 多重比較による差の検定〜
 # aov関数の結果をanova関数に渡します。
 # aov関数の第一引数は連続量~離散量
 aov(口径~分類,data=pot)%>% anova()%>%kable(format="markdown")
-
-```
-
-### TukeyHSD関数で多重比較
-
-次にどの分類同士で差があるのかを調べるために「多重比較」という統計手法を用います。
-いずれの分類でも有意な差を確認できます。
-
-```{r ,warning=FALSE,error=FALSE}
+# TukeyHSD関数で多重比較
 tkh <- 
 	aov(口径 ~ 分類, data = pot) %>% 
 	TukeyHSD() %>% 
@@ -178,33 +186,22 @@ tkh <-
 	as_tibble() %>% 	#tibble_df形式に変換
 	mutate_if(is.numeric, round,3)		#mutate_if()でnumericクラスのカラムにround関数を適用する。
 tkh%>%kable(format="markdown")
-```
 
-
-# 棒グラフを賢く使う
-
-器種や分類のような離散量を可視化する場合には棒グラフを用います。北海道内近世後期の遺跡出土の陶磁器組成のデータを用いて構成比のグラフ表現について考えます。
-
-```{r  fig.width=8, fig.height=6,warning=FALSE,error=FALSE}
+# 円グラフ
 # データ読み込み
-toj<-read.csv("data/pot.csv")
+toj<-read.csv("../data/pot.csv")
 # データの順序定義
 toj$器種<-toj$器種%>%
 	factor(levels=c("碗","皿","その他食膳具","壺･甕･瓶",
 		"土瓶","鍋","すり鉢","灯明皿・油注","その他"))
-toj%>%head()%>%kable()
-```
-
-## 円グラフは使わない
-
-もっとも大切なことは、**円グラフを使わない**ということです。
-人間の目は線の長さや点の位置を把握することには長けていますが、面積の大小や角度を認識するのは苦手です。
-円グラフは面積や円の内角で比率を表現することから、適切な可視化手法とはいえません。
-
-```{r  fig.width=5, fig.height=5,warning=FALSE,error=FALSE}
-toj_pie<-toj%>%group_by(器種)%>%summarise(点数=sum(点数))
+# 円グラフは使わない
+toj_pie <- toj %>% 
+  group_by(器種) %>%
+  summarise(点数=sum(点数))
+pdf("08pie.pdf" )
+par(family = "Japan1GothicBBB" , ps=7)
 pie(toj_pie$点数,labels=toj_pie$器種)
-```
+dev.off()
 
 なお、Rで円グラフ（Pie charts）のヘルプを表示すると次のように記載されています。
 
@@ -228,46 +225,54 @@ Note:
 
 「円グラフで表示できるデータは全てドットチャートで表現できます。円の内角による不正確な判断ではなく、誰もが判断できるモノサシを用いるべきであることを意味しています」（Cleveland 1985,p264）
 
-## ダメ！！絶対〜3D円グラフ〜
-
-3D円グラフは目の錯覚を利用して、特定の値を大きく（小さく）見せる**論外**な手法です。公文書や学術的な報告では**絶対に**使うべきものではありません。
-
-## 構成比棒グラフ
-構成比を比較するために使われるのが構成比棒グラフです。長さや位置によって視覚化されるため、正確な読み取りが可能です。
-構成比棒グラフは比率を比較するための優れたグラフ表現です。
-
-```{r  fig.width=10, fig.height=3.5,warning=FALSE,error=FALSE}
-toj%>%
+## 構成比棒グラフ_カラー
+p <- toj%>%
 	ggplot(aes(x=遺跡名,y=点数,fill=器種))+
 		geom_bar(stat="identity",position="fill")+
 		coord_flip()+
-		scale_fill_ptol()+
-		theme_minimal()
-```
+		scale_fill_viridis_d(
+      guide = guide_legend(
+        keywidth = 1.2, keyheight = 0.8)
+    ) +
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6), 
+  		axis.title= element_blank(),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank()
+  )	
+ggsave("08pot_bar_colour.pdf",p,width=5.8,height=2,family="Japan1GothicBBB") 
 
-## モノクログラフの工夫
 
-発掘調査報告書でカラーグラフが掲載できるケースは稀で、大半はグレースケールで表現されることになります。
-下のグラフはモニター上ではなんとか識別できますが、オフセット印刷の仕上がりでこれを識別することは不可能です。
-凡例との対比は絶望的です。
-
-オフセット印刷の場合、グレスケール（網掛け）は20〜30%スパンが識別できる限界です。したがって、構成比棒グラフでは4群〜5群が表現の限界となります。
-
-```{r  fig.width=10, fig.height=3.5,warning=FALSE,error=FALSE}
-toj%>%
-	ggplot(aes(x=遺跡名,y=点数,fill=器種))+
-		geom_bar(stat="identity",position="fill")+
+## 構成比棒グラフ＿モノクロ
+p <- toj%>%
+  ggplot(aes(x=遺跡名,y=点数,fill=器種))+
+    geom_bar(stat="identity",position="fill", colour="Grey")+
 		coord_flip()+
-		scale_fill_brewer(palette="Greys")+
-		theme_minimal()
-```
+		scale_fill_brewer(
+      palette="Greys", 
+      guide = guide_legend(
+        keywidth = 1.2, keyheight = 0.8
+      )
+     )+
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6), 
+  		axis.title= element_blank(),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank()
+  )	
+ggsave("09pot_bar_bw.pdf",p,width=5.8,height=2,family="Japan1GothicBBB") 
 
 ### 解決法1　カテゴリーを減らす
-
-グラフ表現は複雑な現実をシンプルに割り切って視覚的に表現するためのものです。カテゴリー群が多すぎて識別が困難ならば、カテゴリーを減らすことをまずは考えるべきです。
-3群まで減らせばオフセット印刷でも識別可能なグレースケールのグラフになります。
-
-```{r  fig.width=10, fig.height=3.5,warning=FALSE,error=FALSE}
 # 食膳具、貯蔵具、その他に区分
 toj2<-toj%>%
 	mutate(
@@ -277,105 +282,69 @@ toj2<-toj%>%
 			str_detect(器種,"灯明皿・油注|その他|すり鉢|鍋|土瓶") ~ "その他",
 		)
 	)
-```
-
 # 3区分の構成比棒グラフ
-toj2%>%
+p <- toj2%>%
 	ggplot(aes(x=遺跡名,y=点数,fill=大別器種))+
 		geom_bar(stat="identity",position="fill")+
 		coord_flip()+
 		scale_fill_brewer(palette="Greys")+
-		theme_minimal()
-```
+		theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6), 
+  		axis.title= element_blank(),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank()
+  )	
+ggsave("10pot_bar_bw_3cat.pdf",p,width=5.8,height=2,family="Japan1GothicBBB") 
+
 
 ### 解決法2　ファセットされた棒グラフを使う
-
-どうしてもカテゴリー数を減らしたくない場合は、群変数を器種にとって遺跡ごとにファセットします。花粉分析などの分析結果でよく見る形のグラフです。よほどカテゴリーが多くない限り、表現として成立していますし、オフセット印刷原稿としても対応可能です。
-
-```{r  fig.width=10, fig.height=4,warning=FALSE,error=FALSE}
-toj%>%
+p <- toj%>%
 	ggplot(aes(x=器種,y=点数))+
 	geom_bar(stat="identity")+
-	coord_flip()+facet_wrap(~遺跡名,scales="free")+
-	theme_minimal()
-```
+	coord_flip()+
+  facet_wrap(~遺跡名,scales="free")+
+	theme_minimal() +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6), 
+  		axis.title= element_blank(),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank()
+  )	
+ggsave("11pot_bar_bw_facet.pdf",p,width=5.8,height=2.6,family="Japan1GothicBBB") 
 
 # 散布図で2変量の関係を可視化する
-
-散布図は連続量×連続量の組み合わせのデータで用いられます。考古学の論文・報文でもっとも多く使われるグラフ表現かもしれません。
-しかし、散布図が最も得意とする「**2変量の関係を可視化する**」という用途に使われることが意外に少ないように思います。
-
-## 因果関係を可視化する
-
-「二変量の関係を可視化する」ことの究極の目的は「因果関係の可視化」です。
-
-たとえば、学力と子どもの生活環境の因果関係を統計的に示すなら、「学力テストの点数」という変量「果」（従属変数）に対して「因」となる変量（独立変数）は「親の収入」や「TVの視聴時間」、「睡眠時間」などが考えられます。
-
-したがって、散布図を描く前に考えることは「因果」の「因」にあたる変量（独立変数）と「果」に当たる変量（従属変数）が何か、ということです。
-少なくとも「果」にあたる変量がはっきりしないデータは散布図を描く価値はない、と断言できます。
-
-## 刀身長と他の属性の関係
-
-恵庭西島松5遺跡出土の古代刀剣を対象としたデータを再び使用します。
-追求すべきテーマは「刀身長と他の属性との因果関係」です。
-
-刀身の長さは利用価値に即した刀剣サイズを示すものです。
-刀剣をつくるときには、刀身長が最初に決まり、刀身長に見合った各部のサイズが決められるものと予想されます。
-この場合、因果関係の「果」にあたる変量が刀身長であり、「因」にあたる変量を探索することとなります。
-
-なお、散布図を描く場合の約束として、因果関係の「果」にあたる変量をy軸に、「因」にあたる変量をx軸に割り当てます。
-y軸に割り当てられた「果」にあたる変量を**従属変数**、x軸に割り当てられた「因」にあたる変量を**独立変数**と呼びます。
-
-研究集会ではGGallyパッケージを利用して散布図行列を描画しましたが、PerformanceAnalyticsパッケージを利用して有意性の評価を示しています。
-
-```{r fig.width=10, fig.height=10,warning=FALSE,error=FALSE,message=FALSE}
 library(PerformanceAnalytics)
+pdf("12pair.pdf")
+par(family = "Japan1GothicBBB")
 iron %>%
 	select(全長, 刀身長, 茎長, 刀身先幅, 刀身元幅, 刀身元厚, 茎先幅) %>%
 		chart.Correlation(histogram = TRUE, pch = 19)
-```
+dev.off()
 
-散布図が示すところからは、多くの属性が刀身長と相関関係にあることが読み取れます。
-一方、「刀身元幅」のように非常に強い相関を示す変量もあれば、「茎先幅」のように相関が弱い変量もあります。
-刀身長との相関の強弱を判断することで、古代剣製作にかかる規範意識を読み取ることが可能かもしれません。
-
-## 予測する
-
-散布図を作成する目的は2変量の因果関係を考えることでした。因果関係がわかるということは**予測**ができるということです。次は古代刀剣の刀身元幅から刀身長を予測することを検討します。出土刀剣では刀身が破損せずに出土することはまれですから、元幅から刀身長を予測できれば、出土刀剣の把握に大きな成果がありそうです。
-
-```{r fig.width=10, fig.height=6,warning=FALSE,error=FALSE}
-p<-iron%>%
-	ggplot(aes(x=刀身元幅,y=刀身長))+
-		geom_point()+
-		geom_smooth(method="lm")+
-		theme_minimal()
-
-```
-
-なお、刀身元幅を独立変数とする刀身長の予測式は次のとおりです。
-
-```{r fig.width=10, fig.height=6,warning=FALSE,error=FALSE}
-icoe<-lm(刀身長 ~ 刀身元幅,data=iron)%>%summary()
-icoe$coefficients%>%kable()
-```
-
-y=10.72x-6.28
-
-```{r fig.width=10, fig.height=6,warning=FALSE,error=FALSE}
+## 回帰線つき散布図
 library(ggpmisc)
-iron %>%
+p <- iron %>%
 	ggplot(aes(x=刀身元幅,y=刀身長))+
-		geom_point()+
-		geom_smooth(method="lm")+
+		geom_point(size= 0.8)+
+		geom_smooth(method="lm" , size = 0.5)+
 		theme_minimal() +
-		stat_poly_eq(formula = y ~ x,
+		stat_poly_eq(formula = y ~ x, size =2.5 ,
 			eq.with.lhs = "italic(hat(y))~`=`~",
 			aes(label = paste(stat(eq.label), 
 				stat(rr.label), 
 				sep = "~~~")
 			), parse = TRUE
 		) +
-		stat_fit_glance(label.y = 0.9,
+		stat_fit_glance(label.y = 0.89, size=2.5,
 			method = "lm",
 			method.args = list(formula = y ~ x),
 			aes(label = sprintf(
@@ -383,8 +352,18 @@ iron %>%
 				stat(p.value)
 				)
 			),parse = TRUE
-		)
-```
+		) +
+    theme(
+    	axis.text= element_text(size=5),
+# 	  axis.text.x=element_text(angle=90),
+  		legend.title = element_text(size=6),
+  		legend.text = element_text(size=6), 
+  		axis.title= element_blank(),
+  		plot.title= element_text(size=9),
+  		strip.text=element_text(size=6),
+     panel.grid=element_blank()
+  )	
+ggsave("13scat_lm.pdf",p,width=5.8,height=3.6,family="Japan1GothicBBB") 
 
 
 ```{r echo=FALSE,eval=FALSE}
